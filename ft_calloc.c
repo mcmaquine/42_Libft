e44328fe2222b0bf693884a2139564f5e93fc30d
mcmaquine <mcmaquine@gmail.com>
2025-07-20 22:22:56 -0300

    done ft_calloc ft_strdup

---
+++ ft_calloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaquine <mmaquine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/20 21:23:58 by mmaquine          #+#    #+#             */
+/*   Updated: 2025/07/20 21:24:04 by mmaquine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+The function allocates memory for an array of nmemb elements of size bytes each
+and returns a pointer to the allocated memory. The memory is set to zero. If
+nmemb or size is 0, then ft_calloc() returns a unique pointer value that can
+later be successfully passed to free().
+*/
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	if (nmemb * size == 0)
+		return (malloc(sizeof(void *)));
+	ptr = malloc(nmemb * size);
+	ptr = ft_bzero(ptr, size);
+	return (ptr);
+}
---
+++ ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaquine <mmaquine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/20 21:43:18 by mmaquine          #+#    #+#             */
+/*   Updated: 2025/07/20 21:43:22 by mmaquine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+The function returns a pointer to a new string which is a duplicate of the
+string s. Memory for the new string is obtained with malloc(3), and can be freed
+with free(3). On success, the ft_strup() function returns a pointer to the
+duplicated string. It returns NULL if insufficient memory was available.
+*/
+char	*ft_strdup(const char *s)
+{
+	size_t	slen;
+	char	*dup;
+
+	slen = ft_strlen(s);
+	if (slen == 0)
+		return (NULL);
+	dup = (char *)ft_calloc(slen, sizeof(char));
+	if (!dup)
+		return (NULL);
+	dup = ft_strlcpy(dup, s, slen);
+	return (dup);
+}
